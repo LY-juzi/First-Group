f9ec227417e368fd540d2085ee08e635c16ab539
李景文 <1171720017@qq.com>
2019-12-29 12:15:10 +0800

    黎警文2

--- briup-zhaopin/dev/web/zp/src/views/checkManage/business.vue
+++ briup-zhaopin/dev/web/zp/src/views/checkManage/business.vue
@@ -3,432 +3,352 @@
  * 商家审核页面
  * @Date: 2019-12-23 17:11:53 
  * @Last Modified by: lijingwen
- * @Last Modified time: 2019-12-28 19:12:10
+ * @Last Modified time: 2019-12-29 11:46:59
  */
 <template>
-  
-  <div id="businessCheck">{{businessData}}
-    <div class="searchDiv">
-      <el-select @change="provinceChange" size="mini" v-model="province" clearable placeholder="全部">
-        <el-option
-          v-for="item in provinceData"
-          :key="item.id"
-          :label="item.name"
-          :value="item.name"
-        ></el-option>
-      </el-select>
-    </div>
+  <div id="businessList">
     <div class="tableDiv">
       <el-table
         ref="multipleTable"
         :data="businessList"
         tooltip-effect="dark"
         style="width: 100%"
-        @selection-change="selectionChange"
+        @selection-change="handleSelectionChange"
       >
+      <!-- 建立对应数据的表格 -->
         <el-table-column align="center" type="selection" width="55"></el-table-column>
         <el-table-column align="center" prop="name" label="企业名称"></el-table-column>
         <el-table-column align="center" prop="contactName" label="联系人"></el-table-column>
         <el-table-column align="center" prop="contactPhone" label="联系方式"></el-table-column>
         <el-table-column align="center" prop="industry" label="行业"></el-table-column>
-        <el-table-column align="center" label="所在地">
+        <el-table-column align="center" label="所在地" >
           <template slot-scope="scope">{{scope.row.province}}-{{scope.row.city}}</template>
         </el-table-column>
         <el-table-column align="center" prop="scale" label="公司规模"></el-table-column>
+        <!-- 查看操作，触发相应事件 -->
         <el-table-column align="center" label="详情">
           <template slot-scope="scope">
-            <el-button @click="toSee(scope.row)" type="text" size="small">查看</el-button>
+            <el-button @click="toSee(scope.row)" type="text" size="mini" icon="el-icon-view">查看</el-button>
           </template>
         </el-table-column>
-        <el-table-column align="center" prop="status" label="操作">
-          
+        <!-- 待审核状态操作 -->
+        <el-table-column align="center"  prop="status" label="操作" >
           <template slot-scope="scope">
-            <div v-if="prop === '审核通过'">
-              <div>审核通过</div>
+            <div v-if="scope.row.status === '审核通过'">
+              <div>{{scope.row.status}}</div>
             </div>
-            <div v-else>
-              <el-button type="text" @click="toEdit(scope.row)" size="small">通过</el-button>
-              <el-button type="text" size="small" @click="toDelete(scope.row.id)">拒绝</el-button>
+            <div v-if="scope.row.status === '审核未通过'">
+              <div>{{scope.row.status}}</div>
             </div>
-            
+            <div v-if = "scope.row.status === '待审核'">
+            <el-button  @click="toYes(scope.row)" size="mini"  type="text" >通过</el-button>
+            <el-button type="text" size="mini" @click="toNo(scope.row)" >拒绝</el-button>
+            </div>
           </template>
         </el-table-column>
-        <!-- <el-table-column align="center" label="操作" width="100">
-  
-           <template slot-scope="scope">
-             <el-button type="text" size="small" @click="toDelete(scope.row.id)">通过</el-button>
-            <el-button type="text"  @click="toEdit(scope.row)" size="small">拒绝</el-button> 
-          </template>
-        </el-table-column> -->
-              
       </el-table>
     </div>
-    <div class="footerDiv">
-      <div class="btnDiv">
-        <el-button @click="toBatchDelete" size="mini" type="danger" plain>一键通过</el-button>
+    <!-- 分页操作与一键通过 -->
+   <div class="footerDiv">
+      <!-- 分页操作 -->
+      <div class="pageDiv"> 
+        <el-pagination 
+        @current-change="CurrentChange"
+        :page-size="pageSize"
+        :current-page.sync="currentPage"
+        size="mini"
+        background
+        layout="prev, pager, next"
+        :total="businessData.length">
+        </el-pagination>
       </div>
-      <div class="pageDiv">
-        <el-pagination
-          :page-size="pageSize"
-          :current-page.sync="currentPage"
-          background
-          @current-change="pageChange"
-          layout="prev, pager, next"
-          :total="businessData.length"
-        ></el-pagination>
-      </div>
+    <!-- 一键审核通过 -->
+     <div class="btnDiv">
+       <el-button @click="toadopt()" 
+       type="danger" 
+       round size="mini" >一键通过</el-button>
     </div>
-    <!-- 查看模态框 -->
-    <el-dialog :title="currentBus.name" :visible.sync="seeVisible">
+   </div>
+   <!-- 查看详细数据按钮  -->
+   <el-dialog :title="currentBus.name" :visible.sync="seeVisible">
+      <!-- 行业类型 -->
       <div class="seeDiv">
         <span>行业类型：</span>
         {{currentBus.industry}}
       </div>
+      <!-- 成立时间 -->
       <div class="seeDiv">
         <span>成立时间：</span>
         {{currentBus.establishedTime}}
       </div>
+      <!-- 注册资本 -->
       <div class="seeDiv">
         <span>注册资本：</span>
         {{currentBus.registeredCapital}}
       </div>
+      <!-- 公司规模 -->
       <div class="seeDiv">
         <span>公司规模：</span>
         {{currentBus.scale}}
       </div>
+      <!-- 公司详情 -->
       <div class="descDiv">&nbsp;&nbsp;&nbsp;&nbsp;{{currentBus.description}}</div>
+      <!-- 营业执照 -->
       <div class="imgDiv">
         <a :href="currentBus.businessLicense" target="_blank">
-          <img :src="currentBus.businessLicense" alt width="200" height="150" />
+        <img :src="currentBus.businessLicense" alt width="200" height="150" />
         </a>
       </div>
     </el-dialog>
-    <!-- 修改模态框 -->
-    <!-- <el-dialog title="确定通过审核吗" :visible.sync="editVisible" width="10%">
-       <el-row :gutter="20">
-          <el-col :span="60">
-            <el-form-item prop="location" label="拒绝的理由" :label-width="formLabelWidth">
-              <el-input type="textarea" :rows="4" v-model="currentBus.location"></el-input>
-            </el-form-item>
-          </el-col>
-      </el-row>
-      <div slot="footer" class="dialog-footer">
-        <el-button size="mini" @click="editVisible = false">取 消</el-button>
-        <el-button size="mini" type="primary" @click="toSave('ruleForm')">确 定</el-button>
-      </div>
-    </el-dialog> -->
+  <!-- 通过按钮 -->
+  <el-dialog title="提示" :visible.sync="adoptVisible" style="width:40%">
+      <td>是否确实通过？</td>
+    <div slot="footer" class="dialog-footer" >
+    <el-button @click="adoptVisible = false" type="danger" size="mini">  取消</el-button>
+    <el-button type="primary" @click="toSave" size="mini">确 定</el-button>
+    </div>
+  </el-dialog>
+  <!-- 拒绝按钮 -->
+ <el-dialog title="提示" :visible.sync="refuseVisible" style="width:80%" :before-close="handleClose">
+  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" >
+  <el-form-item label="拒绝理由" prop="name">
+      <el-input type="text" :rows="5" v-model="ruleForm.name"></el-input>
+      <el-button @click="resetForm('ruleForm')" class="Reset" type="text" size="mini">重置</el-button>
+      </el-form-item>
+      </el-form>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="refuse('ruleForm')" size="mini" type="danger">取 消</el-button>
+    <el-button type="primary" @click="torefuse('ruleForm')" size="mini">确 定</el-button>
+  </div>
+ </el-dialog>
   </div>
 </template>
 
 <script>
-import { findAllProvince } from "@/api/province.js";
-import { findAllCity, findCityByProvinceId } from "@/api/city.js";
-import {
-  findAllBusiness,
-  findBusinessByProvince,
-  findBusinessByCity,
-  findBusinessByIndustry,
-  findBusinessByScale,
-  deleteBusinessById,
-  saveOrUpdateBusiness
-} from "@/api/business.js";
+import { findAllBusiness,findBusinessByCity,findBusinessByProvince,findBusinessByScale,findBusinessByIndustry,saveOrUpdateBusiness } from "@/api/business.js";
 import config from "@/utils/config.js";
 export default {
   data() {
-    return {
-      //省份
-      province: "",
-      //省份数组
-      provinceData: [],
+    return {  
+      ruleForm:{name:''},
+      refuseVisible:false,
+      adoptVisible:false,
+      temp:[],
+      input:'',
+      //搜索输入
+      value:'',
+      //页数
+      currentPage:1,
+      //每页条数
+      pageSize:config.pageSize,
+      //状态
+      status:'',
+      //状态数组
+      statusData:[],
       //商家数组
       businessData: [],
-      //商家列表，表格数据
-      // businessList: [],
-      //当前页
-      currentPage: 1,
-      //每页条数
-      pageSize: config.pageSize,
-      //批量删除ids
-      ids: [],
+      
+       statuses: [],
       //当前查看或修改的对象
       currentBus: {},
-      //查看模态框显示与否
+      //模态框显示与否
       seeVisible: false,
-      //修改模态框显示与否
-      editVisible: false,
-      //修改模态框标题名称所占宽度
-      formLabelWidth: "80px",
-      //省份对应的城市信息
-      provinceCityData: [],
-      //状态
-      status:[],
-   
-};
+      rules: {
+          name: 
+            [{ required: true, message: "请输入理由", trigger: "blur" }],
+          }
+    };
   },
   computed: {
-    //分页数据
-    businessList() {
-      let temp = [...this.businessData];
-      let page = this.currentPage;
-      let pageSize = config.pageSize;
-      return temp.slice((page - 1) * pageSize, page * pageSize);
-    }
+    //分页操作
+      //数组展开传递
+       businessList(){
+       let temp =[...this.businessData];
+       let page = this.currentPage;
+       let pageSize = config.pageSize;
+       return temp.slice ((page-1)*pageSize,page*pageSize)
+     },
   },
   methods: {
-    //保存
-    toSave(formName) {
-      this.$refs[formName].validate(async valid => {
-        if (valid) {
-          //通过验证
-          let province = this.currentBus.province;
-          // console.log(province);
-          // console.log(typeof province);
-          // console.log(+province);    NaN  8
-          //如果省份发生更改
-          if (+province) {
-            //将省份id处理为省份名字，过滤省份数组
-            let result = this.provinceData.filter(item => {
-              return item.id === +province;
-            })[0];
-            // result 是省份对象
-            this.currentBus.province = result.name;
-          }
-          //保存
-          try {
-            let res = await saveOrUpdateBusiness(this.currentBus);
-            if (res.status === 200) {
-              this.findAllBus();
-              this.editVisible = false;
-              config.successMsg(this, "修改成功");
-            } else {
-              config.errorMsg(this, "修改失败");
-            }
-          } catch (error) {
-            console.log(error);
-            config.errorMsg(this, "修改失败");
-          }
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
+    handleClose(){
+      this.refuseVisible=false;
+      this.$refs["ruleForm"].resetFields();
     },
-    //模态框省份发生改变
-    async dialogProChange(val) {
-      // console.log(val);
-      //重置城市下拉列表
-      this.currentBus.city = "";
-      //通过省份id获取城市
-      try {
-        let res = await findCityByProvinceId({ provinceId: val });
-        this.provinceCityData = res.data;
-      } catch (error) {
-        console.log(error);
-        config.errorMsg(this, "通过省份查找城市失败");
-      }
+    refuse(formName){
+      this.refuseVisible=false;
+      this.$refs[formName].resetFields();
     },
-    // 页数发生改变
-    pageChange(page) {
-      this.currentPage = page;
-    },
-    //省份发生改变
-    async provinceChange(val) {
-      this.city = "";
-      this.industry = "";
-      this.scale = "";
-      //val 是option的value值
-      if (val) {
-        try {
-          let res = await findBusinessByProvince({ province: val });
-          this.businessData = res.data;
-          this.currentPage = 1;
-        } catch (error) {
-          config.errorMsg(this, "通过省份查找商家信息错误");
-        }
-      } else {
-        this.findAllBus();
-      }
-    },
-    //城市发生改变
-    async cityChange(val) {
-      this.province = "";
-      this.industry = "";
-      this.scale = "";
-      //val 是option的value值
-      if (val) {
-        try {
-          let res = await findBusinessByCity({ city: val });
-          this.businessData = res.data;
-          this.currentPage = 1;
-        } catch (error) {
-          config.errorMsg(this, "通过城市查找商家信息错误");
-        }
-      } else {
-        this.findAllBus();
-      }
-    },
-    //行业发生改变
-    async industryChange(val) {
-      this.province = "";
-      this.city = "";
-      this.scale = "";
-      //val 是option的value值
-      if (val) {
-        try {
-          let res = await findBusinessByIndustry({ industry: val });
-          this.businessData = res.data;
-          this.currentPage = 1;
-        } catch (error) {
-          config.errorMsg(this, "通过行业查找商家信息错误");
-        }
-      } else {
-        this.findAllBus();
-      }
-    },
-    //规模发生改变
-    async scaleChange(val) {
-      this.province = "";
-      this.city = "";
-      this.industry = "";
-      //val 是option的value值
-      if (val) {
-        try {
-          let res = await findBusinessByScale({ scale: val });
-          this.businessData = res.data;
-          this.currentPage = 1;
-        } catch (error) {
-          config.errorMsg(this, "通过规模查找商家信息错误");
-        }
-      } else {
-        this.findAllBus();
-      }
+    //分页操作
+    CurrentChange(val){
+      this.currenPage = val;
     },
     //查看
     toSee(row) {
       this.currentBus = { ...row };
       this.seeVisible = true;
     },
-    //批量通过
-    toBatchDelete() {
-      //获取要批量删除的id  this.ids
-      let ids = this.ids;
-      if (ids.length > 0) {
-        this.$alert("是全部通过？", "提示", {
-          confirmButtonText: "通过",
+    //通过
+    toYes(row) {
+      this.currentBus = {...row};
+      this.adoptVisible =true;
+    },
+    //拒绝
+    toNo(row) {
+      this.currentBus = {...row};
+      this.refuseVisible =true;
+    },
+    //取消下拉框关键字时，刷新页面
+    valueChange(val){
+      if(val){
+      }else{
+        this.value=''
+        this.findAllBus();
+        this.input=null;
+      }
+    },
+     torefuse(formName){
+      this.$refs[formName].validate(async(valid) => {
+          if (valid) {
+            this.currentBus.status="审核未通过"
+      try {
+        let res =await saveOrUpdateBusiness(this.currentBus);
+        this.$refs[formName].resetFields();
+        if(res.status===200){
+          config.successMsg(this,'成功'); 
+        this.findAllBus();
+        this.refuseVisible =false;
+        }else{
+          config.errorMsg(this,'失败')
+        }
+        
+      } catch (error) {
+        config.errorMsg(this,'失败')
+      }
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+    },
+    resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
+    async toSave(){
+      try {
+        this.currentBus.status="审核通过"
+        let res =await saveOrUpdateBusiness(this.currentBus);
+        if(res.status===200){
+          config.successMsg(this,'成功'); 
+        this.findAllBus();
+        this.adoptVisible =false;
+        }else{
+          config.errorMsg(this,'失败')
+        }
+        
+      } catch (error) {
+        config.errorMsg(this,'失败')
+      }
+    },
+    //一键通过
+    toadopt(){
+      let statuses = this.statuses
+      if (statuses.length>0) {
+         this.$alert('是否通过', '提示', {
+          confirmButtonText: '确定',
           callback: action => {
-            if (action === "confirm") {
-              let result = [];
-              ids.forEach(async id => {
+           if (action==='confirm') {
+             let result = [];
+             statuses.forEach(async(item)=>{
+               item.status='审核通过'
+               console.log(statuses)
                 try {
-                  let res = await deleteBusinessById({ id: id });
-                  result.push(res.status);
-                } catch (error) {
-                  result.push(500);
-                }
-              });
-              setTimeout(() => {
-                // console.log(result);
-                //判断是否都是200
-                let resu = result.every(item => {
-                  return item === 200;
-                });
-                if (resu) {
-                  config.successMsg(this, "批量通过成功");
-                } else {
-                  config.errorMsg(this, "批量通过失败");
-                }
-                this.findAllBus();
-              }, 2000);
-            }
+                    let res =await saveOrUpdateBusiness(item);
+                    
+                    result.push(res.status);
+                    } catch (error) {
+                      config.errorMsg(this,'失败')
+                      }
+              })
+             setTimeout(()=>{
+               let resu = result.every((item)=>{
+                 return item ===200;
+               })
+               if(resu){
+                 config.successMsg(this,'成功');
+                 this.findAllBus();
+               }
+             },1000)
+           }
           }
         });
       } else {
-        this.$message({
-          message: "请选中要删除的数据",
-          type: "warning"
+         this.$message({
+          message: "请选中要通过的商家",
+          type: "text"
         });
       }
     },
     //复选框选中切换
-    selectionChange(val) {
-      //val 就是选中的对象组成的数组
-      // console.log(val);
-      let ids = val.map(item => {
-        return item.id;
-      });
-      this.ids = ids;
-    },
-    //查找所有省份信息
-    async findAllPro() {
-      try {
-        let res = await findAllProvince();
-        this.provinceData = res.data;
-      } catch (error) {
-        config.errorMsg(this, "查找错误");
-      }
-    },
-    //查找所有城市信息
-    async findAllCi() {
-      try {
-        let res = await findAllCity();
-        this.cityData = res.data;
-      } catch (error) {
-        config.errorMsg(this, "查找错误");
-      }
+    handleSelectionChange(val) {
+      this.statuses = val;
     },
     //查找所有的商家信息
     async findAllBus() {
       try {
         let res = await findAllBusiness();
-        this.businessData = res.data;
-        // this.status =res.data.status;
-        this.currentPage = 1;
-        //行业数组
-        let industryArr = res.data.map(item => {
-          return item.industry;
+        this.temp = res.data;
+        this.selectStatus();
+        //状态数组
+        let statusArr = this.businessData.map(item => {
+          return item.status;
         });
         //去重
-        this.industryData = [...new Set(industryArr)];
-        //规模数组
-        let scaleArr = res.data.map(item => {
-          return item.scale;
-        });
-        //去重
-        this.scaleData = [...new Set(scaleArr)];
+        this.statusData = [...new Set(statusArr)];
       } catch (error) {
-        config.errorMsg(this, "查找错误");
+        config.errorMsg(this,'查找失败')
       }
-    }
+    },
+    selectStatus(){
+      this.businessData = [];
+      this.temp.forEach(item=>{
+        if('待审核'===item.status){
+          this.businessData.push(item);
+        }
+      })
+    },
   },
+ 
+ 
   created() {
-    this.findAllPro();
-    this.findAllCi();
     this.findAllBus();
   },
   mounted() {}
 };
 </script>
-<style lang="scss" scoped>
+<style scoped>
 .tableDiv {
   margin-top: 10px;
 }
-.footerDiv {
+.box{
+  float: right
+}
+.footerDiv{
   overflow: hidden;
   margin-top: 10px;
-  .btnDiv {
-    float: left;
-  }
-  .pageDiv {
-    float: right;
-  }
+}
+.btnDiv{
+  float: left;
+}
+.pageDiv{
+  float: right;
+}
+.Reset{
+  float: right;
 }
 .seeDiv {
   border-bottom: 1px solid #ccc;
   line-height: 30px;
   font-weight: bold;
-  span {
-    color: #777;
-  }
 }
 .descDiv {
   font-weight: bold;
@@ -441,8 +361,8 @@ export default {
 .imgDiv {
   text-align: center;
 }
-.dialog-footer {
+.dialog-footer{
   text-align: center;
-  margin-top: -30px;
+  margin-top: -40px;
 }
 </style>
